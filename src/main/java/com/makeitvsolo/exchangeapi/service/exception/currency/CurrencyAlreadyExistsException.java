3f4b494bfc1626a5950c5983b00ecef4ead78d8e
makeitvsolo <makeitvsolo@gmail.com>
2023-09-12 14:00:16 +0300

    style: CurrencyAlreadyExistsException

--- src/main/java/com/makeitvsolo/exchangeapi/service/exception/currency/CurrencyAlreadyExistsException.java
+++ src/main/java/com/makeitvsolo/exchangeapi/service/exception/currency/CurrencyAlreadyExistsException.java
@@ -5,6 +5,7 @@ import com.makeitvsolo.exchangeapi.core.exception.ExchangeApiException;
 import java.text.MessageFormat;
 
 public final class CurrencyAlreadyExistsException extends ExchangeApiException {
+
     public CurrencyAlreadyExistsException(String code) {
         super(MessageFormat.format("Currency with code {0} already exists", code));
     }
