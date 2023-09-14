e737b89226b9083471f5c4ad0aa1e2a8364bb99c
makeitvsolo <makeitvsolo@gmail.com>
2023-09-14 09:42:45 +0300

    fix: ValidatedCreateCurrency

--- src/main/java/com/makeitvsolo/exchangeapi/service/validation/currency/ValidatedCreateCurrency.java
+++ src/main/java/com/makeitvsolo/exchangeapi/service/validation/currency/ValidatedCreateCurrency.java
@@ -2,15 +2,14 @@ package com.makeitvsolo.exchangeapi.service.validation.currency;
 
 import com.makeitvsolo.exchangeapi.core.validation.ValidatedBefore;
 import com.makeitvsolo.exchangeapi.service.dto.currency.CreateCurrencyDto;
-import com.makeitvsolo.exchangeapi.service.dto.currency.CurrencyCodeDto;
 
 public final class ValidatedCreateCurrency implements ValidatedBefore<CreateCurrencyDto> {
-    private final ValidatedBefore<CurrencyCodeDto> code;
+    private final ValidatedBefore<String> code;
     private final ValidatedBefore<String> fullName;
     private final ValidatedBefore<String> sign;
 
     ValidatedCreateCurrency(
-            ValidatedBefore<CurrencyCodeDto> code, ValidatedBefore<String> fullName, ValidatedBefore<String> sign
+            ValidatedBefore<String> code, ValidatedBefore<String> fullName, ValidatedBefore<String> sign
     ) {
         this.code = code;
         this.fullName = fullName;
@@ -28,6 +27,6 @@ public final class ValidatedCreateCurrency implements ValidatedBefore<CreateCurr
 
     @Override
     public CreateCurrencyDto validated() {
-        return new CreateCurrencyDto(code.validated().value(), fullName.validated(), sign.validated());
+        return new CreateCurrencyDto(code.validated(), fullName.validated(), sign.validated());
     }
 }
