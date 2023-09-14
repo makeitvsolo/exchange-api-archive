ec22ced3e0157d7bb1ad86322c4c3151901858db
makeitvsolo <makeitvsolo@gmail.com>
2023-09-14 10:39:47 +0300

    fix: Currency tests

--- src/test/java/com/makeitvsolo/exchangeapi/domain/CurrencyTests.java
+++ src/test/java/com/makeitvsolo/exchangeapi/domain/CurrencyTests.java
@@ -1,7 +1,23 @@
 package com.makeitvsolo.exchangeapi.domain;
 
+import com.makeitvsolo.exchangeapi.domain.exception.InvalidCurrencyCodeException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 @DisplayName("Currency")
 public class CurrencyTests {
+
+    @Test
+    @DisplayName("code should be in ISO 4217 format")
+    public void codeShouldBeInISOFormat() {
+        var wrongCode = "wrong code";
+
+        Assertions.assertThrows(
+                InvalidCurrencyCodeException.class,
+                () -> Currency.from(UUID.randomUUID(), wrongCode, "Wrong Currency", "Wrong Sing")
+        );
+    }
 }
