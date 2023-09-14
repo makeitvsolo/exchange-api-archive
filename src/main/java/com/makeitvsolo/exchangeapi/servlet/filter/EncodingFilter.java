ce646564440a78185af5d5d7cb7f2756084f9294
makeitvsolo <makeitvsolo@gmail.com>
2023-09-14 13:42:38 +0300

    feat: impl `filter` in `servlet`

---
+++ src/main/java/com/makeitvsolo/exchangeapi/servlet/filter/EncodingFilter.java
@@ -0,0 +1,27 @@
+package com.makeitvsolo.exchangeapi.servlet.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public final class EncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        servletResponse.setCharacterEncoding("UTF-8");
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setContentType("application/json");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
